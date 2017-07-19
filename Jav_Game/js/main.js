e162a43fc8cee6c3d5c764492a6f2080631dba69
Allan <allan00hernan@gmail.com>
2017-07-19 18:04:47 -0400

    d

--- Jav_Game/js/main.js
+++ Jav_Game/js/main.js
@@ -192,7 +192,7 @@ PlayState.create = function () {
     };
 
     // create level
-    this.game.add.image(0, 0, 'background');
+    this.game.add.image(20, 220, 'background');
     this._loadLevel(this.game.cache.getJSON(`level:${this.level}`));
 
     // crete hud with scoreboards)
